92b7c1f15175f8aa6a7f0feacf639a54167a747f
Aleksandr <shilov.alexandr92@mail.ru>
2021-09-03 00:15:57 +0500

    hw2 is finished

---
+++ src/test/java/com/epam/tc/hw2/DriverInit.java
@@ -0,0 +1,26 @@
+package com.epam.tc.hw2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class DriverInit {
+
+    protected WebDriver webDriver;
+
+    @BeforeMethod
+    public void setupDriver() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void clear() {
+        webDriver.close();
+    }
+}
