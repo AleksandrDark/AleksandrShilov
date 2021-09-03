7a75675f362fbb7712540e7067a6a0454c478977
Aleksandr <shilov.alexandr92@mail.ru>
2021-09-03 23:24:28 +0500

    I expanded DataProvider classes and changed test Classes

--- src/test/java/com/epam/tc/hw3/testlogic/DifferentElementsPageTest.java
+++ src/test/java/com/epam/tc/hw3/testlogic/DifferentElementsPageTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.epam.tc.hw3.pages.BasePage;
 import com.epam.tc.hw3.pages.DifferentElementsPage;
 import com.epam.tc.hw3.pages.LoginPage;
+import com.epam.tc.hw3.util.DataProviderHelper;
+import com.epam.tc.hw3.util.DataProviderPage;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,19 +17,18 @@ import org.testng.annotations.Test;
 
 public class DifferentElementsPageTest extends BasePage {
 
-    @Test
-    public void testDifferentElementsOnPage() {
+    @Test(dataProvider = "getTextPages", dataProviderClass = DataProviderPage.class)
+    public void testDifferentElementsOnPage(DataProviderHelper providerHelper) {
         LoginPage loginPage = new LoginPage(webDriver);
         //1. Open test site by URL
         loginPage.openSite();
         assertThat(webDriver.getCurrentUrl()).isEqualTo(URL);
         //2. Assert Browser title "Home Page"
-        assertThat(webDriver.getTitle()).isEqualTo("Home Page");
+        assertThat(webDriver.getTitle()).isEqualTo(providerHelper.getTitle());
         //3. Perform login
         loginPage.performLogin();
         //4. Assert User name in the left-top side of screen is "ROMAN IOVLEV"
-        WebElement webElement = webDriver.findElement(By.id("user-name"));
-        assertThat(loginPage.getUsername()).isEqualTo("ROMAN IOVLEV");
+        assertThat(loginPage.getUsername()).isEqualTo(providerHelper.getUsername());
         //5. Open through the header menu Service -> Different Elements Page
         DifferentElementsPage differentElementsPage = new DifferentElementsPage(webDriver);
         differentElementsPage.getHeaderMenu().click();
@@ -39,12 +40,11 @@ public class DifferentElementsPageTest extends BasePage {
         differentElementsPage.getRadioSelen().click();
         //8. Select in dropdown "Yellow"
         Select color = new Select(differentElementsPage.getDropdownYellow());
-        color.selectByVisibleText("Yellow");
+        color.selectByVisibleText(providerHelper.getYellow());
         //9. Assert that checkbox, radio button, radio button names are corresponding to selected
-        List<String> expectedContentInLogs = Arrays.asList("Yellow", "Selen", "Wind", "Water");
         List<String> textLogs = differentElementsPage.getLogs().stream().map(WebElement::getText)
             .collect(Collectors.toList());
-        assertThat(textLogs).asString().containsSubsequence(expectedContentInLogs);
+        assertThat(textLogs).asString().containsSubsequence(providerHelper.getExpectedContentInLogs());
     }
 
 }
--- src/test/java/com/epam/tc/hw3/util/DataProvider.java
+++
@@ -1,8 +0,0 @@
-package com.epam.tc.hw3.util;
-
-public class DataProvider {
-
-    public static Object[][] getTextHomePage() {
-        return new Object[][]{{"Home Page"}};
-    }
-}
