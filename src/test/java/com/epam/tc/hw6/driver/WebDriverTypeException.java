172c7d23cec6a5763a66ffddebae71abefa0a9d7
AleksandrDark <shilov.alexandr92@mail.ru>
2021-11-10 12:59:07 +0500

    deleted files

--- src/test/java/com/epam/tc/hw6/driver/WebDriverTypeException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.tc.hw6.driver;
-
-public class WebDriverTypeException extends RuntimeException {
-
-    public WebDriverTypeException(String message) {
-        super(message);
-    }
-}
